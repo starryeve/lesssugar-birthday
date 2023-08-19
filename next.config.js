c884341f014103b34b7c87c48c38ecd41666e27f
starryeve <958259016@qq.com>
2023-08-19 23:01:01 +0800

    fix: custom url prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
+// set url prefix for the static resource, useful in deploying to the custom domain or cdn
+let assetPrefix = ``
+let basePath = ``
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') // remove the github owner
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true,
