eff85dea180bb19d9641201c6817673730f20680
starryeve <958259016@qq.com>
2023-08-19 14:35:56 +0000

    Deploying to gh-pages from @ starryeve/lesssugar-birthday@28b93390ac044838954327dca978d0d0e8b4a2be 🚀

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-// import { DndProvider } from 'react-dnd'
-// import { HTML5Backend } from 'react-dnd-html5-backend'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
