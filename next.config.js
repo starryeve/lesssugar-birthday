e08001b887b7d437d92aa20b10c635de7f7769e2
starryeve <958259016@qq.com>
2023-08-19 23:25:56 +0800

    fix: no need for  prefix in the case of custom domain

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@
 let assetPrefix = ``
 let basePath = ``
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') // remove the github owner
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') // remove the github owner
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 const nextConfig = {
   assetPrefix,
