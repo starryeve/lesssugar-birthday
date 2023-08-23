4683387fa16b3da492c009d6dd38dd063077329f
starryeve <958259016@qq.com>
2023-08-23 14:34:47 +0000

    Deploying to gh-pages from @ starryeve/lesssugar-birthday@3c7b2396734768295e515edb35342f2422091799 🚀

---
+++ _next/static/FQ6xQFge9TATeoVd3ANE6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/794-e12fe10a103efbf7.js","static/css/bc34e4e88b057c26.css","static/chunks/pages/index-1b985cac7b96da26.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
