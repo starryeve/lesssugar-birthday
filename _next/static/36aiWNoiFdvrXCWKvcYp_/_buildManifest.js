9dda13a8d9d749b90d450e7b5829b737a0a68391
starryeve <958259016@qq.com>
2023-08-23 04:29:18 +0000

    Deploying to gh-pages from @ starryeve/lesssugar-birthday@6b812d79d0a98e2781b068a7e37e7ff8752eb8aa 🚀

---
+++ _next/static/36aiWNoiFdvrXCWKvcYp_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/535-3c5e8a289c82adca.js","static/css/d9f65888e36f5a38.css","static/chunks/pages/index-9f3b899b91a9303e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/lTdlKx8vP1J0P4f7KfoIm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/535-3c5e8a289c82adca.js","static/css/d9f65888e36f5a38.css","static/chunks/pages/index-67f3532d44ac95a6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
